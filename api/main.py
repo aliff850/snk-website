d9755a6cfd0f9dd4da146f21c9c34477c53f0b1f
0xLighted <Thundermagicreal@gmail.com>
2025-09-29 00:11:15 +0800

    - Added cors to api - Updated some configuration

--- api/main.py
+++ api/main.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI
 from api.utils.mudah import mudahRouter
 from api.utils.zigwheels import informationRouter
 
+from fastapi.middleware.cors import CORSMiddleware
+
 VERSION = "0.1.1"
 
 app = FastAPI(debug=True, version=VERSION, title=f"Vehicle Valuation API | v{VERSION}", root_path='/api')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to your Vercel URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(mudahRouter)
 app.include_router(informationRouter)
+
+print("Started API")
