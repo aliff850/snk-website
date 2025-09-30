2a34d52a17ca14da6eed8819e07e0e10a9b01df4
aliff850 <aliff.ramly@gmail.com>
2025-09-30 15:41:26 +0800

    Fixed middleware redirecting to login page globally

---
+++ app/editor/page.tsx
@@ -0,0 +1,11 @@
+export default function ArticleEditor() {
+
+    return(
+
+        <div className="h-screen flex flex-col justify-center items-center bg-[url('/images/w214.jpg')] bg-cover bg-center font-onest">
+            <p className="text-3xl font-bold">This will be the article management system basically</p>
+        </div>
+
+    )
+
+}
