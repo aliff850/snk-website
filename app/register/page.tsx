e2936ea293bb04b64a246d74f94a6ae4c258db72
0xLighted <thundermagicreal@gmail.com>
2025-10-25 17:55:08 +0800

    wrapped login and register page in suspense

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,14 @@
 import RegisterPage from "./RegisterPage";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Registration",
 };
 
 export default function Register() {
-  return <RegisterPage />;
+  return (
+    <Suspense fallback={<div className="w-full min-h-svh bg-[url('/images/w214.jpg')] bg-cover bg-center flex items-center justify-center text-white">Loading...</div>}>
+      <RegisterPage />
+    </Suspense>
+  );
 }
